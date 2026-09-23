76b7ad8a50984279791a059a33bd61baa639cf95
eoray777 <rrcsf@discente.ifpe.edu.br>
2026-09-23 14:12:16 -0300

    Create Color.kt

---
+++ app/src/main/ifpe/agroplay/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package br.edu.ifpe.agroplay.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Cor principal definida no PRD do Agro Play
+val AgroPlayPrimary = Color(0xFF73E841)
+
+// Cores complementares/padrão do Material 3 (temporárias/placeholder)
+val AgroPlaySecondary = Color(0xFF53634E)
+val AgroPlayTertiary = Color(0xFF386568)
+
+val PrimaryDark = Color(0xFF58CB26)
+val SecondaryDark = Color(0xFFBBCBB2)
+val TertiaryDark = Color(0xFFA1CED1)
