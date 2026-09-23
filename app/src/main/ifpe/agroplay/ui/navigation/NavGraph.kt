3ac6aeb0a4dd55c86aa5b86888ba610cc5120478
eoray777 <rrcsf@discente.ifpe.edu.br>
2026-09-23 14:09:24 -0300

    Create NavGraph.kt

---
+++ app/src/main/ifpe/agroplay/ui/navigation/NavGraph.kt
@@ -0,0 +1,47 @@
+package br.edu.ifpe.agroplay.ui.navigation
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun NavGraph(
+    modifier: Modifier = Modifier,
+    startDestination: String = NavTarget.Home.route
+) {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = startDestination,
+        modifier = modifier
+    ) {
+        composable(NavTarget.Home.route) {
+            HomeScreen()
+        }
+    }
+}
+
+@Composable
+fun HomeScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Agro Play",
+            fontSize = 32.sp,
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colorScheme.primary
+        )
+    }
+}
